ddd6439ac796bba2c0475d751f3b246a4b149d3b
Elena Datsenko <elenasheglova15414@gmail.com>
2024-01-07 23:08:13 -0800

    done

--- src/test/java/ru/netology/page/VerificationPage.java
+++ src/test/java/ru/netology/page/VerificationPage.java
@@ -6,7 +6,7 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 
 public class VerificationPage {
-    private final SelenideElement codeField = $(".notification__content");
+    private final SelenideElement codeField = $("[data-test-id='code'] input");
     private final SelenideElement verifyButton = $("[data-test-id='action-verify']");
     private final SelenideElement errorMessage = $("[data-test-id='error-notification'] .notification__content");
 
