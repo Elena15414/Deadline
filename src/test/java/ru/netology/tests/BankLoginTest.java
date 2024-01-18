4285ec0258c22f9734ac3e7f711dbec149576a8d
Elena Datsenko <elenasheglova15414@gmail.com>
2024-01-17 22:59:33 -0800

    CI

--- src/test/java/ru/netology/tests/BankLoginTest.java
+++ src/test/java/ru/netology/tests/BankLoginTest.java
@@ -1,10 +1,12 @@
 package ru.netology.tests;
 
 import org.junit.jupiter.api.*;
+import ru.netology.data.DataHelper;
 import ru.netology.page.LoginPage;
 
 import static com.codeborne.selenide.Selenide.open;
-import static ru.netology.data.DataHelper.getAuthData;
+import static ru.netology.data.DataHelper.*;
+import static ru.netology.data.DataHelper.generateRandomUser;
 import static ru.netology.data.SQLHelper.*;
 
 public class BankLoginTest {
@@ -32,6 +34,26 @@ public class BankLoginTest {
         var verificationPage = loginPage.validLogin(getAuthData());
         verificationPage.verificationPageIsVisible();
         verificationPage.validVerify(String.valueOf(getVerificationCode()));
+    }
+    @Test
+    @DisplayName("Should get error when invalid login")
+    void shouldNotLoginWithInvalidLogin() {
+        loginPage.validLogin(new DataHelper.AuthData(testName, generateRandomPassword()));
+        loginPage.findErrorMessage("Неверно указан логин или пароль");
+    }
+
+    @Test
+    @DisplayName("Should get error when invalid password")
+    void shouldNotLoginWithInvalidPassword() {
+        loginPage.validLogin(new DataHelper.AuthData(generateRandomLogin(), testPassword));
+        loginPage.findErrorMessage("Неверно указан логин или пароль");
+    }
+
+    @Test
+    @DisplayName("Should get error when invalid login and password")
+    void shouldNotLoginWithInvalidLoginAndPassword() {
+        loginPage.validLogin(generateRandomUser());
+        loginPage.findErrorMessage("Неверно указан логин или пароль");
     }
 
 }
