24716e09856c85ef1fd9dd66094ee5b7d20b402b
Elena Datsenko <elenasheglova15414@gmail.com>
2024-01-17 23:35:01 -0800

    tests

--- src/test/java/ru/netology/page/DashboardPage.java
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -2,8 +2,9 @@ package ru.netology.page;
 
 import com.codeborne.selenide.SelenideElement;
 
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
 
 public class DashboardPage {
     private SelenideElement personalAccount = $("[data-test-id='dashboard']");
